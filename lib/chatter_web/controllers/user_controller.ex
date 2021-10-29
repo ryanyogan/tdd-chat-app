c11ae62015b8aaeb7db576cd2546755c03dffd1f
ryan <ryanyogan@gmail.com>
2021-10-29 09:26:35 -0500

    Refactorings to clean up tests, and boundary layers

--- lib/chatter_web/controllers/user_controller.ex
+++ lib/chatter_web/controllers/user_controller.ex
@@ -1,23 +1,17 @@
 defmodule ChatterWeb.UserController do
   use ChatterWeb, :controller
 
-  alias Doorman.Auth.Secret
   alias Doorman.Login.Session
-  alias Chatter.{User, Repo}
+  alias Chatter.{Accounts, User}
 
   def new(conn, _params) do
-    changeset = User.changeset(%User{}, %{})
+    changeset = Accounts.change_user(%User{})
 
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"user" => params}) do
-    changeset =
-      %User{}
-      |> User.changeset(params)
-      |> Secret.put_session_secret()
-
-    case Repo.insert(changeset) do
+    case Accounts.create_user(params) do
       {:ok, user} ->
         conn
         |> Session.login(user)
